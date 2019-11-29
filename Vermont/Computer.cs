92b2fb1869c406f29237c62833d14493fa33f45c
Alessandro Rapiti <a.rapiti@vetrya.com>
2019-11-29 21:55:43 +0100

    empty

--- Vermont/Computer.cs
+++ Vermont/Computer.cs
@@ -87,6 +87,6 @@ namespace Vermont
     {
         public decimal Result => decimal.Parse(this.Entry);
         public string Entry { get; }
-        public Context(string entry) => this.Entry = entry;
+        public Context(string entry) => this.Entry = entry.Replace(" ", string.Empty);
     }
 }
