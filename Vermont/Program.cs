d2f0b3170a2217d13689ecfde7dc4a5e92041c89
Alessandro Rapiti <a.rapiti@vetrya.com>
2019-11-30 11:30:22 +0100

    ReadLine

--- Vermont/Program.cs
+++ Vermont/Program.cs
@@ -9,8 +9,8 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            //string ready = Console.ReadLine(); //2+5+6+9*5
-            string ready = "2*(5+ (6  +9)*2)*4/2";
+            string ready = Console.ReadLine(); //"2*(5+ (6  +9)*2)*4/2"
+            //string ready = "2*(5+ (6  +9)*2)*4/2";
             Computer computer = new Computer();
             Context context = computer.Calculate(new Context(ready));
             Console.WriteLine(context.Result);
